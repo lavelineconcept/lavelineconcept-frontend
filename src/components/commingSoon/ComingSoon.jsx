67f371c64bcdad72d8b40cbe9828f5f0411442a4
your_name <orkunkorcak@gmail.com>
2026-02-13 01:35:49 +0300

    proje iskeleti ve comingsoon sayfası oluşturuldu

---
+++ src/components/commingSoon/ComingSoon.jsx
@@ -0,0 +1,32 @@
+import logo from '../../assets/lavelineconcept-logosmall.png';
+import styles from './ComingSoon.module.css';
+
+const ComingSoon = () => {
+    return (
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <img src={logo} alt="La Véline Concept Logo" className={styles.logoImage} />
+                <h1 className={styles.logoText}>La Véline Concept</h1>
+            </header>
+
+            <div className={styles.divider}></div>
+
+            <main>
+                <h2 className={styles.title}>Çok Yakında Sizlerle</h2>
+                <p className={styles.description}>
+                    En özel günleriniz için özenle tasarlanan nişan hediyeliklerimizle çok yakında buradayız.
+                </p>
+                <p className={styles.description}>
+                    La Véline Concept - Detaylarda Saklı Zarafet
+                </p>
+
+            </main>
+
+            <footer className={styles.footer}>
+                <p>© 2026 La Véline Concept. Tüm hakları saklıdır.</p>
+            </footer>
+        </div>
+    );
+};
+
+export default ComingSoon;
