cf255dfaf8d326ae54045a00128597ed08e439f8
your_name <orkunkorcak@gmail.com>
2026-02-27 11:53:51 +0300

    kayan yazı oluşturuldu

--- src/components/slideText/slideText.jsx
+++ src/components/slideText/slideText.jsx
@@ -1,10 +1,29 @@
 import css from "./slideText.module.css";
 
 export default function SlideText() {
+    const items = [
+        "TOPLU SİPARİŞ",
+        "LOREM IPSUM DOLOR SIT AMET",
+        "YENİ KOLEKSİYON",
+        "1500TL ÜZERİ KARGO ÜCRETSİZ",
+        "LOREM IPSUM DOLOR SIT AMET",
+        "GÜVENLI ÖDEME",
+        "EL YAPIMI",
+        "LOREM IPSUM DOLOR SIT AMET"
+    ];
+
+    // Double the items for a seamless loop
+    const displayItems = [...items, ...items, ...items];
+
     return (
         <section className={css.slideText}>
-            <div className={css.brand}>
-                <h1 className={css.title}>LOREM IPSUM DOLOR SIT AMET</h1>
+            <div className={css.brandScroll}>
+                {displayItems.map((item, index) => (
+                    <div key={index} className={css.brand}>
+                        <h1 className={css.title}>{item}</h1>
+                        <span className={css.separator}>•</span>
+                    </div>
+                ))}
             </div>
         </section>
     );
