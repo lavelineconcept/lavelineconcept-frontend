8a4ca62d1fba864c5b12293bf6dabddf43876427
your_name <orkunkorcak@gmail.com>
2026-03-11 14:42:59 +0300

    kategori animasyonu eklendi

--- src/components/categories/categories.jsx
+++ src/components/categories/categories.jsx
@@ -1,7 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCategories } from "../../redux/categories/operations";
 import { selectCategories } from "../../redux/categories/selectors";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import css from "./categories.module.css";
 
 export default function Categories() {
@@ -12,26 +17,50 @@ export default function Categories() {
     dispatch(fetchCategories());
   }, [dispatch]);
 
+  if (!categories || categories.length === 0) return null;
+
   return (
     <section className={css.categoriesSection}>
       <div className={css.container}>
         <h2 className={css.title}>Kategoriler</h2>
-        <div className={css.grid}>
+        
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          spaceBetween={0}
+          slidesPerView={1}
+          breakpoints={{
+            768: {
+              slidesPerView: "auto",
+              centeredSlides: false,
+            }
+          }}
+          loop={true}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false,
+          }}
+          pagination={{ clickable: true }}
+          navigation={true}
+          className={css.mySwiper}
+          watchSlidesProgress={true}
+        >
           {categories.map((category) => (
-            <div key={category._id} className={css.card}>
-              <div className={css.imageWrapper}>
-                <img
-                  src={category.image}
-                  alt={category.name}
-                  className={css.image}
-                />
-                <div className={css.overlay}>
-                  <h3 className={css.categoryName}>{category.name}</h3>
+            <SwiperSlide key={category._id} className={css.slide}>
+              <div className={css.card}>
+                <div className={css.imageWrapper}>
+                  <img
+                    src={category.image}
+                    alt={category.name}
+                    className={css.image}
+                  />
+                  <div className={css.overlay}>
+                    <h3 className={css.categoryName}>{category.name}</h3>
+                  </div>
                 </div>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
     </section>
   );
